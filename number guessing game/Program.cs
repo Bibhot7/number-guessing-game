79309733cff7ce979fb2744e4dafc782a0930256
Bibhot7 <bibhot7@outlook.com>
2024-01-25 13:56:17 +1300

    Add project files.

---
+++ number guessing game/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Channels;
+
+namespace number_guessing_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isCorrectguess = false;
+            Random random = new Random();
+            int randomNum = random.Next(1, 11);
+
+            Console.WriteLine("Welcome to my number guessing game");
+            Console.WriteLine("A number between 1 and 10 will be generated.");
+            Console.WriteLine("If you guess the correct number you will win");
+
+            while (!isCorrectguess)
+            {
+                Console.WriteLine("please guess a number.");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess > randomNum)
+                {
+                    Console.WriteLine("your guess is too high");
+                }
+                else if(guess < randomNum)
+                {
+                    Console.WriteLine("your guess is too low ");
+                }
+                else
+                {
+                    Console.WriteLine("your guess is correct");
+                    isCorrectguess = true;
+                }
+            }
+            Console.WriteLine("congratulations you won the game!!!");
+        }
+    }
+}
